c27f7c5c7cd851dcb46cd6c10eabe69f613e9955
Michael Penkov <misha.penkov@gmail.com>
2017-09-10 12:02:01 +0900

    Implement encoding/decoding helper for ASCII, JIS and Unicode
    https://gist.github.com/hMatoba/efdfd5c0d13a4ee1ca2d1d4094c53a31

--- tests/s_test.py
+++ tests/s_test.py
@@ -767,10 +767,72 @@ class UTests(unittest.TestCase):
         self.assertRaises(ValueError, load_user_comment(binary))
 
 
+class HelperTests(unittest.TestCase):
+    def test_headers(self):
+        """Are our headers the correct length?"""
+        self.assertEqual(len(_helper._ASCII_PREFIX), _helper._PREFIX_SIZE)
+        self.assertEqual(len(_helper._JIS_PREFIX), _helper._PREFIX_SIZE)
+        self.assertEqual(len(_helper._UNICODE_PREFIX), _helper._PREFIX_SIZE)
+        self.assertEqual(len(_helper._UNDEFINED_PREFIX), _helper._PREFIX_SIZE)
+
+    def test_encode_ascii(self):
+        """Do we encode ASCII correctly?"""
+        text = 'hello world'
+        expected = b'\x41\x53\x43\x49\x49\x00\x00\x00hello world'
+        actual = _helper.dump(text, encoding='ascii')
+        self.assertEqual(expected, actual)
+
+    def test_decode_ascii(self):
+        """Do we decode ASCII correctly?"""
+        binary = b'\x41\x53\x43\x49\x49\x00\x00\x00hello world'
+        expected = 'hello world'
+        actual = _helper.load(binary)
+        self.assertEqual(expected, actual)
+
+    def test_encode_jis(self):
+        """Do we encode JIS correctly?"""
+        text = '\u3053\u3093\u306b\u3061\u306f\u4e16\u754c'
+        expected = b'\x4a\x49\x53\x00\x00\x00\x00\x00' + text.encode('shift_jis')
+        actual = _helper.dump(text, encoding='jis')
+        self.assertEqual(expected, actual)
+
+    def test_decode_jis(self):
+        """Do we decode JIS correctly?"""
+        expected = '\u3053\u3093\u306b\u3061\u306f\u4e16\u754c'
+        binary = b'\x4a\x49\x53\x00\x00\x00\x00\x00' + expected.encode('shift_jis')
+        actual = _helper.load(binary)
+        self.assertEqual(expected, actual)
+
+    def test_encode_unicode(self):
+        """Do we encode Unicode correctly?"""
+        text = '\u3053\u3093\u306b\u3061\u306f\u4e16\u754c'
+        expected = b'\x55\x4e\x49\x43\x4f\x44\x45\x00' + text.encode('utf_16_be')
+        actual = _helper.dump(text, encoding='unicode')
+        self.assertEqual(expected, actual)
+
+    def test_decode_unicode(self):
+        """Do we decode Unicode correctly?"""
+        expected = '\u3053\u3093\u306b\u3061\u306f\u4e16\u754c'
+        binary = b'\x55\x4e\x49\x43\x4f\x44\x45\x00' + expected.encode('utf_16_be')
+        actual = _helper.load(binary)
+        self.assertEqual(expected, actual)
+
+    def test_encode_bad_encoding(self):
+        """De we gracefully handle bad input when encoding?"""
+        self.assertRaises(ValueError, _helper.dump, 'hello world', 'koi-8r')
+
+    def test_decode_bad_encoding(self):
+        """De we gracefully handle bad input when decoding?"""
+        self.assertRaises(ValueError, _helper.load, b'\x00\x00\x00\x00\x00\x00\x00\x00hello')
+        self.assertRaises(ValueError, _helper.load, b'\x12\x34\x56\x78\x9a\xbc\xde\xffhello')
+        self.assertRaises(ValueError, _helper.load, b'hello world')
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTests([unittest.makeSuite(UTests),
-                    unittest.makeSuite(ExifTests)])
+                    unittest.makeSuite(ExifTests),
+                    unittest.makeSuite(HelperTests)])
     return suite
 
 
